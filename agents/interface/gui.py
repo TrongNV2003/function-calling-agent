7e3a632eaa3ee361b8e3362d8ab59b1e31ce5cac
TrongNV2003 <trongnguyen220603@gmail.com>
2025-03-08 17:25:56 +0700

    perf: update UI to perform reasoning of agents

--- agents/interface/gui.py
+++ agents/interface/gui.py
@@ -1,12 +1,8 @@
-import requests
+import time
 import streamlit as st
-
-from agents.config.yaml_loader import load_config
 from agents.service.pipeline import AgentPipeline
 
-config = load_config()
 ppl = AgentPipeline()
-API_URL = config["app"]["host"]
 
 st.set_page_config(
     page_title="Agento",
@@ -19,7 +15,7 @@ st.set_page_config(
 st.markdown("""
     <style>
         .chat-container {
-            max-width: 800px;
+            max-width: 1000px;
             margin: 0 auto;
         }
         .user-message, .bot-message {
@@ -42,6 +38,44 @@ st.markdown("""
             font-size: 18px;
             align-self: flex-start;
         }
+        .thinking-step {
+            background-color: rgba(80, 80, 80, 0.5);
+            padding: 10px;
+            border-radius: 1.5rem;
+            margin: 5px 0;
+            opacity: 0.7;
+            max-width: 70%;
+        }
+        .action-step {
+            background-color: rgba(0, 100, 0, 0.5);
+            padding: 10px;
+            border-radius: 1.5rem;
+            margin: 5px 0;
+            opacity: 0.7;
+            max-width: 70%;
+        }
+        .execution-step {
+            background-color: rgba(0, 0, 100, 0.5);
+            padding: 10px;
+            border-radius: 1.5rem;
+            margin: 5px 0;
+            opacity: 0.7;
+            max-width: 70%;
+        }
+        .final-answer {
+            background-color: rgba(50, 50, 50, .85);
+            padding: 10px;
+            border-radius: 1.5rem;
+            margin: 5px 0;
+            max-width: 70%;
+        }
+        .error-step {
+            background-color: rgba(255, 0, 0, 0.5);
+            padding: 10px;
+            border-radius: 1.5rem;
+            margin: 5px 0;
+            max-width: 70%;
+        }
         .chat {
             display: flex;
             flex-direction: column;
@@ -49,120 +83,51 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# Chatbot params
 st.markdown("# :rainbow[Agento v1]")
 st.sidebar.header("Agent")
 selected_bot = st.sidebar.selectbox("Select chatbot:", 
                                     options=["Chatbot Basic", "Chatbot RAG"],
                                     label_visibility="collapsed")
 
-st.sidebar.subheader("Temperature")
-temperature = st.sidebar.slider(
-    "Độ sáng tạo của RAG-E:", 
-    min_value=0.0, max_value=1.0, value=0.0, step=0.1,
-)
-
-st.sidebar.subheader("Threshold")
-threshold = st.sidebar.slider(
-    "Độ giới hạn kiến thức retrieval:", 
-    min_value=0.0, max_value=2.0, value=1.0, step=0.2,
-)
-
-def Chatbot_RAG():
-    # Upsert PART
-    st.sidebar.markdown("---")
-    st.sidebar.subheader("Select document types")
-    selected_document = st.sidebar.selectbox("Chọn thể loại document muốn upsert:", 
-                                        options=["Văn bản", "Thơ"])
-    st.sidebar.markdown("---")
-    st.sidebar.subheader("Select Upsert types")
-    selected_upsert = st.sidebar.selectbox("Chọn kiểu upsert:", 
-                                        options=["Upsert text", "Upsert file"])
-    
-    if selected_upsert == "Upsert text":
-        st.sidebar.subheader("Upsert text")
-        input_text = st.sidebar.text_area("Enter here:", 
-                                placeholder="Nhập text cần upsert...",
-                                label_visibility="collapsed")
-
-        if st.sidebar.button("Upsert text"):
-            if input_text:
-                with st.spinner("Đang xử lý..."):
-                    response = requests.post(
-                        f"{API_URL}/upsert-text",
-                        json={"index_name": config["elasticsearch"]["index_name"], "text_input": input_text}
-                    )
-                    if response.status_code == 200:
-                        st.sidebar.write("Dữ liệu đã được upsert thành công.")
-                    else:
-                        st.sidebar.write("Lỗi khi gọi API:", response.status_code)
-            else:
-                st.write("Vui lòng nhập đầy đủ thông tin.")
-    elif selected_upsert == "Upsert file":
-        st.sidebar.subheader("Upsert file")
-        uploaded_file = st.sidebar.file_uploader("Drag file pdf here:", 
-                                        type=["pdf"],
-                                        label_visibility="collapsed")
-        if st.sidebar.button("Upsert file"):
-            with st.spinner("Đang xử lý..."):
-                response = requests.post(
-                    f"{API_URL}/upsert-file?doc_type={selected_document}",
-                    files={"file_path": uploaded_file}
-                )
-                if response.status_code == 200:
-                    st.sidebar.write("Dữ liệu đã được upsert từ file.")
-                else:
-                    st.sidebar.write("Lỗi khi gọi API:", response.status_code)
-
-    # Delete Index PART
-    st.sidebar.markdown("---")
-    st.sidebar.header("Delete Index")
-    index_to_delete = st.sidebar.text_input("Nhập tên index để xoá:",
-                                            placeholder='text_embeddings')
-    if st.sidebar.button("Xoá Index"):
-        if index_to_delete:
-            response = requests.delete(
-                f"{API_URL}/delete-index?index_name={index_to_delete}"
-            )
-            if response.status_code == 200:
-                st.sidebar.write("Index đã được xoá thành công.")
-            else:
-                st.sidebar.write("Lỗi khi gọi API:", response.status_code)
-        else:
-            st.sidebar.write("Vui lòng nhập tên index.")
+st.sidebar.subheader("Show prompt in response")
+show_prompt = st.sidebar.selectbox("Visualize prompt:", 
+                                    options=["True", "False"],
+                                    label_visibility="collapsed") == "True"
 
 
-def query_processing(query_text):
+def query_processing(query_text, container):
+    steps = []
+
+    def display_step(step):
+        steps.append(step)
+
+        html_content = ""
+        for s in steps:
+            if s["type"] == "thinking":
+                html_content += f'<div class="thinking-step">Bước {s["step"]}: Suy nghĩ - {s["content"]}</div>'
+            elif s["type"] == "action":
+                html_content += f'<div class="action-step">Bước {s["step"]}: Hành động - {s["content"]}</div>'
+            elif s["type"] == "execution":
+                html_content += f'<div class="execution-step">Bước {s["step"]}: Thực thi - {s["content"]}</div>'
+            elif s["type"] == "final_answer":
+                html_content += f'<div class="final-answer">Kết quả cuối cùng: {s["content"]}</div>'
+            elif s["type"] == "error":
+                html_content += f'<div class="error-step">Lỗi: {s["content"]}</div>'
+
+        container.markdown(html_content, unsafe_allow_html=True)
+
+        time.sleep(0.5)
+
     with st.spinner("Đang xử lý..."):
-        response = ppl.run(query_text)
-        
-    return response
-
-def health_check():
-    st.sidebar.markdown("---")
-    st.sidebar.header("Health Check")
-    if st.sidebar.button("Kiểm tra trạng thái"):
-        st.sidebar.write("I am fine! 👍🏻")
+        ppl.run(query_text, step_callback=display_step, show_prompt=show_prompt)
 
 
-#Sending processing
 def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
         initial_bot_message = "Hello! I am Agento. How can I assist you today?"
         st.session_state.chat_history.append({"role": "assistant", "content": initial_bot_message})
-        
-    query_text = st.chat_input("Ask Agento something...")
-    if query_text:
-        with st.spinner("Thinking..."):
-            # selected_bot = st.sidebar.selectbox("Select Bot", ["Chatbot RAG", "Chatbot Text"])
 
-            bot_response = ppl.run(query_text)
-        if bot_response:
-            st.session_state.chat_history.append({"role": "user", "content": query_text})
-            st.session_state.chat_history.append({"role": "assistant", "content": bot_response})
-
-    # Display chat
     st.markdown('<div class="chat-container">', unsafe_allow_html=True)
     for message in st.session_state.chat_history:
         if message["role"] == "user":
@@ -171,16 +136,27 @@ def main():
             st.markdown(f'<div class="chat"><div class="bot-message">{message["content"]}</div></div>', unsafe_allow_html=True)
     st.markdown('</div>', unsafe_allow_html=True)
 
+    query_text = st.chat_input("Ask Agento something...")
+    if query_text:
+        st.session_state.chat_history.append({"role": "user", "content": query_text})
+        st.markdown(f'<div class="chat-container"><div class="chat"><div class="user-message">{query_text}</div></div></div>', unsafe_allow_html=True)
+        
+        response_container = st.empty()
+        
+        query_processing(query_text, response_container)
+
+
+def health_check():
+    st.sidebar.markdown("---")
+    st.sidebar.header("Health Check")
+    if st.sidebar.button("Kiểm tra trạng thái"):
+        st.sidebar.write("I am fine! 👍🏻")
 
 if __name__ == "__main__":
     main()
     
-    # Display UI selectors
-    if selected_bot == "Chatbot RAG":
-        Chatbot_RAG()
-    
-    # Health check
     health_check()
     
 
 # streamlit run streamlit_app.py
+# python -m streamlit run interface/gui.py
